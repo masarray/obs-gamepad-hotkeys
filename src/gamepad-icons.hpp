a42a7b6f91d13f47ba2208229ce02c3c6bbb42ec
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-08-10 15:25:37 +0700

    ui: add Lucide action icon API

--- src/gamepad-icons.hpp
+++ src/gamepad-icons.hpp
@@ -7,6 +7,9 @@
 namespace ogh {
 
 QIcon lucideGamepadIcon(const QPalette &palette);
+QIcon lucideTrash2Icon(const QPalette &palette);
+QIcon lucideRefreshCwIcon(const QPalette &palette);
+QIcon lucidePlusIcon(const QPalette &palette);
 QIcon gamepadControlIcon(const QString &control, const QPalette &palette);
 
 } // namespace ogh
